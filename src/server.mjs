1864e07be51d52e4972e293c9f10115cad6c2122
Melvin Kempe <k3mp314@gmail.com>
2025-05-27 11:49:14 +0200

    [Add] block.mjs

---
+++ src/server.mjs
@@ -0,0 +1,31 @@
+import { app } from './app.mjs';
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE = `http://localhost:${DEFAULT_PORT}`;
+let NODE_PORT;
+
+
+const synchronize = async () => {
+  const response = await fetch(`${ROOT_NODE}/api/blocks`);
+  if (response) {
+    const result = await response.json();
+    console.log('Replacing chain on sync with:', result.data.chain);
+    blockChain.replaceChain(result.data.chain);
+  }
+};
+
+if (process.env.GENERATE_NODE_PORT === 'true') {
+  NODE_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = NODE_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.log(
+    `Servern är startad på adress ${PORT} och kör i läget ${process.env.NODE_ENV}`
+  );
+
+  if (PORT !== DEFAULT_PORT) {
+    synchronize();
+  }
+});
