55c97cc407c284f5da89db01b7e20af4cf0d648d
Melvin Kempe <k3mp314@gmail.com>
2025-05-30 09:45:50 +0200

    [Add] the getBlockByIndex function

--- src/controllers/blockchain-controllers.mjs
+++ src/controllers/blockchain-controllers.mjs
@@ -16,3 +16,16 @@ export const addBlock = (req, res) => {
     .status(201)
     .json({ success: true, message: "Block is added", data: blockChain.chain });
 };
+
+export const getBlockByIndex = (req, res) => {
+  const { index } = req.params;
+  const block = blockChain.chain[index];
+
+  if (!block)
+    return res.status(404).json({
+      success: false,
+      message: "Block not found",
+    });
+
+  res.status(200).json({ success: true, message: "Block found", data: block });
+};
--- src/routes/blockchain-routes.mjs
+++ src/routes/blockchain-routes.mjs
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   addBlock,
+  getBlockByIndex,
   listAllBlocks,
 } from "../controllers/blockchain-controllers.mjs";
 
 const routes = Router();
 
 routes.get("/", listAllBlocks);
+routes.get("/:index", getBlockByIndex);
 routes.post("/mine", addBlock);
 
 export default routes;
